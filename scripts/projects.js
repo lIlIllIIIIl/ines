8c7f5d7609177b4e4332eb2e94a658a628d25f08
lIlIllIIIIl <alexis.kessab@orange.fr>
2021-10-11 13:35:05 +0200

    Initial commit

---
+++ scripts/projects.js
@@ -0,0 +1,14 @@
+const page = document.querySelector("body")
+
+const projects = page.querySelectorAll(".single_work")
+
+// projects_list = [
+//     ["../images/chaise.jpg", "3D JPN [KORIUM]", "DATE: September 2021", "ROLE: 3D, artistic direction, webdesign, prototyping", "WITH: Soliman Al Halaby"],
+//     ["../images/chaise.jpg", "3D JPN [KORIUM]", "DATE: September 2021", "ROLE: 3D, artistic direction, webdesign, prototyping", "WITH: Soliman Al Halaby"],
+// ]
+
+// for (i in projects_list){
+//     // console.log(projects[i]);
+//     const image = projects[i].querySelector(".project_image")
+//     console.log(image);
+// }
