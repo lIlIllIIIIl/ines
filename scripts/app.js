1f0ad629cc0e135fb80a6f47a8f0a25b519d06fe
Alexis Kessab <alexis.kessab@orange.fr>
2021-11-09 08:53:12 +0100

    _

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,8 @@ const page = document.querySelector("body")
 
 const containers = page.querySelectorAll(".container")
 
+const footer = page.querySelector("footer")
+
 const explore = page.querySelector(".explore")
 const menu = page.querySelector(".main_menu")
 const open_menu = page.querySelector(".container_menu")
@@ -11,14 +13,14 @@ menu.addEventListener('click', (e)=>{
 		open_menu.classList.replace("close", "open")
 		for (let i = 0 ; i < 4 ; i++){
 			containers[i].style.display="none"
-			// console.log(containers[i]);
+			footer.style.display="none";
 		}
 		open_menu.style.display="block"
 	}else if(open_menu.classList[2] === "open"){
 		open_menu.classList.replace("open", "close")
 		for (let i = 0 ; i < 4 ; i++){
 			containers[i].style.display="block"
-			// console.log(containers[i]);
+			footer.style.display="flex";
 		}
 		open_menu.style.display="none"
 	}
