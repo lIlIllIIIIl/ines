bb8ad25d1bd8a9face2171aa394e281b1384e76e
Alexis Kessab <alexis.kessab@orange.fr>
2022-04-21 14:09:32 +0200

    (:

--- public/scripts/scroll.js
+++ public/scripts/scroll.js
@@ -3,23 +3,69 @@ var __webpack_exports__ = {};
 /*!*******************************!*\
   !*** ./src/scripts/scroll.js ***!
   \*******************************/
-var lastPanel = document.querySelector(".container_contact");
-gsap.utils.toArray(".section-parallax .parallax-content").forEach(function (section, i) {
-  var heightDiff = section.offsetHeight - section.parentElement.offsetHeight;
-  gsap.fromTo(section, {
-    x: -heightDiff
-  }, {
-    scrollTrigger: {
-      trigger: section,
-      endTrigger: lastPanel,
-      scrub: true,
-      start: "top center",
-      end: "top center" // markers: true,
-
-    },
-    x: 0,
-    ease: "none"
-  });
+var panel1 = document.querySelector("#panel-1 .parallax-content");
+var heightDiff1 = panel1.offsetHeight - panel1.parentElement.offsetHeight;
+gsap.fromTo(panel1, {
+  x: -heightDiff1
+}, {
+  scrollTrigger: {
+    trigger: panel1,
+    endTrigger: panel1,
+    scrub: true,
+    start: "top center",
+    end: "bottom center",
+    markers: true
+  },
+  x: 0,
+  ease: "none"
 });
+var panel3 = document.querySelector("#panel-2 .parallax-content");
+var heightDiff3 = panel3.offsetHeight - panel3.parentElement.offsetHeight;
+gsap.fromTo(panel3, {
+  x: -heightDiff3
+}, {
+  scrollTrigger: {
+    trigger: panel3,
+    endTrigger: panel3,
+    scrub: true,
+    start: "top center",
+    end: "bottom center",
+    markers: true
+  },
+  x: 0,
+  ease: "none"
+});
+var panel2 = document.querySelector("#panel-3 .parallax-content");
+var heightDiff2 = panel2.offsetHeight - panel2.parentElement.offsetHeight;
+gsap.fromTo(panel2, {
+  x: -heightDiff2
+}, {
+  scrollTrigger: {
+    trigger: panel2,
+    endTrigger: panel2,
+    scrub: true,
+    start: "top center",
+    end: "bottom center",
+    markers: true
+  },
+  x: 0,
+  ease: "none"
+}); // gsap.utils.toArray(".section-parallax .parallax-content").forEach((section, i) => {
+// 	const heightDiff = section.offsetHeight - section.parentElement.offsetHeight;
+// 	gsap.fromTo(section,{ 
+// 	x: -heightDiff
+// 	}, {
+// 	scrollTrigger: {
+// 		trigger: section,
+// 		endTrigger: lastPanel,
+// 		scrub: true,
+// 		start: "top center",
+// 		end: "top center",
+// 		// markers: true,
+// 	},
+// 	x: 0,
+// 	ease: "none"
+// 	});
+// });
 /******/ })()
 ;
--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,19 +1,77 @@
-const lastPanel = document.querySelector(".container_contact")
+const panel1 = document.querySelector("#panel-1 .parallax-content")
 
-gsap.utils.toArray(".section-parallax .parallax-content").forEach((section, i) => {
-	const heightDiff = section.offsetHeight - section.parentElement.offsetHeight;
-	gsap.fromTo(section,{ 
-	x: -heightDiff
-	}, {
-	scrollTrigger: {
-		trigger: section,
-		endTrigger: lastPanel,
-		scrub: true,
-		start: "top center",
-		end: "top center",
-		// markers: true,
-	},
-	x: 0,
-	ease: "none"
-	});
+const heightDiff1 = panel1.offsetHeight - panel1.parentElement.offsetHeight;
+gsap.fromTo(panel1,{ 
+x: -heightDiff1
+}, {
+scrollTrigger: {
+	trigger: panel1,
+	endTrigger: panel1,
+	scrub: true,
+	start: "top center",
+	end: "bottom center",
+	markers: true,
+},
+x: 0,
+ease: "none"
 });
+
+
+const panel3 = document.querySelector("#panel-2 .parallax-content")
+
+const heightDiff3 = panel3.offsetHeight - panel3.parentElement.offsetHeight;
+gsap.fromTo(panel3,{ 
+x: -heightDiff3
+}, {
+scrollTrigger: {
+	trigger: panel3,
+	endTrigger: panel3,
+	scrub: true,
+	start: "top center",
+	end: "bottom center",
+	markers: true,
+},
+x: 0,
+ease: "none"
+});
+
+
+
+
+const panel2 = document.querySelector("#panel-3 .parallax-content")
+
+const heightDiff2 = panel2.offsetHeight - panel2.parentElement.offsetHeight;
+gsap.fromTo(panel2,{ 
+x: -heightDiff2
+}, {
+scrollTrigger: {
+	trigger: panel2,
+	endTrigger: panel2,
+	scrub: true,
+	start: "top center",
+	end: "bottom center",
+	markers: true,
+},
+x: 0,
+ease: "none"
+});
+
+
+
+// gsap.utils.toArray(".section-parallax .parallax-content").forEach((section, i) => {
+// 	const heightDiff = section.offsetHeight - section.parentElement.offsetHeight;
+// 	gsap.fromTo(section,{ 
+// 	x: -heightDiff
+// 	}, {
+// 	scrollTrigger: {
+// 		trigger: section,
+// 		endTrigger: lastPanel,
+// 		scrub: true,
+// 		start: "top center",
+// 		end: "top center",
+// 		// markers: true,
+// 	},
+// 	x: 0,
+// 	ease: "none"
+// 	});
+// });
